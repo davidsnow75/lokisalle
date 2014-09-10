99dee836904d1aa6ac962cbe89bad802b43792f4
Erwan Thomas <janotus@maen.fr>
2014-09-11 00:02:32 +0200

    réécriture du contrôleur frontal

--- web/core/Application.php
+++ web/core/Application.php
@@ -7,103 +7,98 @@
  * modules adéquats (c'est le contrôleur frontal)
  */
 
-class Application
-{
-    private $url        = null;
-    private $controller = null;
-    private $action     = null;
-    private $parameter1 = null;
-    private $parameter2 = null;
-    private $parameter3 = null;
+class Application {
 
-    /**
-     * L'application commence ici:
-     * L'url, décomposée en une hiérarchie du type "AGENT/ACTION/PARAMÈTRES",
-     * permet de lancer les modules adéquats au traitement de la requête
-     */
+    private $url         = null;
+    private $controller  = null;
+    private $action      = null;
+    private $parameters  = null;
+
     public function __construct()
     {
-        // remplit les propriétés de l'objet
+        // On peuple d'abord les attributs de l'application de façon adéquate
         $this->url = $this->analyser_url();
 
-        // ETAPE 0: A-t-on demandé une url ?
-        if ( !is_null($this->controller) ) {
 
-            // ÉTAPE 1: le contrôleur demandé existe-t-il ?
-            if ( file_exists('./controllers/' . $this->controller . '_ctrl.php') ) {
+        ## TEST 0 ## Sans url requise, on envoie la page d'accueil
+        if ( is_null($this->url) ) {
+            require './controllers/HomeController.php';
+            $home_ctrl = new HomeController;
+            $home_ctrl->index();
 
-                // oui, alors on le crée en instanciant sa classe
-                require_once './controllers/' . $this->controller . '_ctrl.php';
-                $this->controller = new $this->controller();
+            return; // rien d'autre à faire ici, on quitte le constructeur
+        }
 
-                // ÉTAPE 2: la méthode demandée est-elle disponible dans ce contrôleur ?
-                if ( method_exists($this->controller, $this->action) ) {
 
-                    // oui, alors on l'appelle avec les éventuels arguments
-                    // NOTE: on commence à tester par le dernier argument possible car on sait que s'il existe,
-                    // alors tous les précédents existent nécessairement aussi
-                    if ( isset($this->parameter3) ) {
-                        // on obtient qqchose du genre: $this->home->method($param_1, $param_2, $param_3);
-                        $this->controller->{$this->action}($this->parameter1, $this->parameter2, $this->parameter3);
+        ## TEST 1 ## Si le contrôleur demandé existe, alors on en crée une instance
+        if ( file_exists('./controllers/' . $this->controller . 'Controller.php') ) {
+            require './controllers/' . $this->controller . 'Controller.php';
+            $thisController = $this->controller . 'Controller';
+            $this->controller = new $thisController;
 
-                    } elseif ( isset($this->parameter2) ) {
-                        // semblable à au-dessus
-                        $this->controller->{$this->action}($this->parameter1, $this->parameter2);
+        } else { // l'url demandée n'a aucun sens, on envoie la page 404
+            require_once './controllers/ErrorController.php';
+            $error_ctrl = new ErrorController;
+            $error_ctrl->notFound($this->url);
 
-                    } elseif ( isset($this->parameter1) ) {
-                        // idem
-                        $this->controller->{$this->action}($this->parameter1);
+            return; // rien d'autre à faire ici, on quitte le constructeur
+        }
 
-                    } else {
-                        // pas de paramètres, la méthode est appelée seule, comme cela par ex.: $this->home->method();
-                        $this->controller->{$this->action}();
-                    }
-                } else {
-                    // la méthode n'est pas disponible, on se rabat par défaut sur la méthode index(),
-                    // que tout contrôleur est tenu d'implémenter
-                    $this->controller->index();
-                }
-            } else {
-                // l'url est tout à fait invalide, on envoie la page 404
-                require_once './controllers/error_ctrl.php';
-                $erreur = new Error;
-                $erreur->notFound($this->url);
-            }
-        } else {
-            // pas d'url fournie, donc on appelle le contrôleur "d'accueil"
-            // NOTE: les trois instructions suivantes sont du type de celles éventuellement exécutées
-            // par les instructions ci-dessus
-            require_once './controllers/home_ctrl.php';
-            $home = new Home;
-            $home->index();
+
+        ## TEST 2 ## Si l'action demandée existe dans ce contrôleur
+        if ( method_exists($this->controller, $this->action) ) {
+
+            // si aucun paramètre n'a été fourni, on se contente de lancer l'action seule
+            if ( empty($this->parameters) ):
+                $this->controller->{$this->action}();
+
+            else: // sinon on fournit les paramètres à l'action
+                $this->controller->{$this->action}( $this->parameters );
+
+            endif;
+
+        } else { // l'action demandée n'existe pas, on actionne celle par défaut (index)
+            $this->controller->index();
+
+            return; // rien d'autre à faire ici, on quitte le constructeur
         }
     }
 
+
     /**
-     * Récupère et décomposer l'url pour la préparer avant son traitement
+     * Récupérer et décomposer l'url pour peupler les propriétés vitales de l'application
+     *
+     * @param void
+     * @return mixed (string ou null)
      */
-    private function analyser_url()
-    {
+    private function analyser_url() {
+
         // $_GET['url'] est une pseudo-url, dont l'intérêt par rapport à un système
         // avec plusieurs $_GET est notamment d'obliger à une hiérarchisation des
         // paramètres de routage (le premier étant forcément le contrôleur, le
         // second l'action, etc.)
-        if (isset($_GET['url'])) {
+        if (isset( $_GET['url'] )) {
 
             // décomposer l'url demandée en autant de partie que de slash
-            $url = trim($_GET['url'], '/'); // supprime les caractères invisibles & les slashs de début & fin de chaîne
-            $url = filter_var($url, FILTER_SANITIZE_URL); // supprime les caractères illégaux d'une url
-            $url = explode('/', $url); // décompose
+            $url = trim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url_array = explode('/', $url);
 
             // assigne aux propriétés adéquates les différentes parties (éventuelles) de l'url
-            $this->controller = (isset($url[0]) ? $url[0] : null);
-            $this->action     = (isset($url[1]) ? $url[1] : null);
-            $this->parameter1 = (isset($url[2]) ? $url[2] : null);
-            $this->parameter2 = (isset($url[3]) ? $url[3] : null);
-            $this->parameter3 = (isset($url[4]) ? $url[4] : null);
+            $url_depth = count($url_array);
+            for ($i = 0; $i < $url_depth; $i++) {
+                switch ($i) {
+                    case 0:  $this->controller    = ucfirst( $url_array[0] );  break;
+                    case 1:  $this->action        = $url_array[1];  break;
+                    default: $this->parameters[]  = $url_array[$i]; break;
+                }
+            }
 
-            // renvoie $url pour rendre possible sa transmission éventuelle au contrôleur
+            // renvoie $url pour rendre possible sa transmission éventuelle au contrôleur (ex. page 404)
             return $url;
         }
+
+        // on renvoie null car aucune url n'a été fournie via $_GET['url']
+        return null;
     }
 }
