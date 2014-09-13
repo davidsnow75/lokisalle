7bb364fc0213cd0aa495935d5dcc259f7d768555
Erwan Thomas <janotus@maen.fr>
2014-09-13 22:01:43 +0200

    Mise en conformité avec cahier des charges

--- web/views/_templates/footer.php
+++ web/views/_templates/footer.php
@@ -1,6 +1,13 @@
 <footer class="main-footer blc">
   <div class="ctn">
-    <div class="valign"><a href="#">Mentions légales</a></div>
+    <ul>
+      <li><a href="#">Mentions légales</a></li>
+      <li><a href="#">C.G.V.</a></li>
+      <li><a href="#">Plan du site</a></li>
+      <li><a href="#">Imprimer la page</a></li>
+      <li><a href="#">S'inscrire à la newsletter</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
   </div>
 </footer>
 
