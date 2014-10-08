305c221877cf649c36d60893fa88eaebb7232fa9
Erwan Thomas <janotus@maen.fr>
2014-10-09 01:49:46 +0200

    Quasi finalisation des produits, avant tests

--- models/Produit.php
+++ models/Produit.php
@@ -19,31 +19,54 @@ class Produit extends Model
     protected $salle_id;
     protected $promo_id;
 
-
-    /* ce constructeur permet ou bien d'instancier un produit déjà existant en BDD
-     * (en lui fournissant son id), ou bien d'initialiser un produit vide, qu'il
-     * faudra remplir manuellement.
+    /**
+     * Crée un nouveau produit ou en récupère un de la BDD
+     *
+     * ce constructeur permet ou bien d'instancier un produit déjà existant en BDD
+     * (en lui fournissant son id), ou bien d'initialiser un nouveau produit avec un
+     * tableau de données le renseignant.
+     *
+     * @param array pour la création d'un nouveau produit, int pour la récupération via la BDD
+     * @throws Exception si l'initialisation échoue, avec un message décrivant l'erreur
+     * @return void
      */
-    public function __construct( $id = false )
+    public function __construct( $arg )
     {
-        if ( $id ) {
-            $sql = "SELECT * FROM produits WHERE id='" . intval($id) . "';";
+        /* on a passé un tableau, c'est donc qu'on veut créer un produit */
+        if ( is_array( $arg ) ) {
+            $creation_produit_msg = $this->setProduit( $arg );
 
-            $result = $this->exequery($sql);
-
-            if ( $result->num_rows != 0 ) {
-
-                $produit = $result->fetch_assoc();
-
-                $this->id           = $produit[0]['id'];
-                $this->date_arrivee = $produit[0]['date_arrivee'];
-                $this->date_depart  = $produit[0]['date_depart'];
-                $this->prix         = $produit[0]['prix'];
-                $this->etat         = $produit[0]['etat'];
-                $this->salle_id     = $produit[0]['salles_id'];
-                $this->promo_id     = $produit[0]['promotions_id'];
+            /* si qqch a été retourné par setProduit(), alors c'est qu'il y a eu une erreur */
+            if ( $creation_produit_msg ) {
+                throw new Exception( $creation_produit_msg );
             }
+
+            return; // l'objet a été correctement initialisé, on s'arrête là
         }
+
+        /* Si le paramètre n'était pas un tableau, alors on va supposer qu'on demande la
+         * récupération d'un produit de la BDD.
+         */
+        $sql = "SELECT * FROM produits WHERE id='" . intval($arg) . "';";
+
+        $result = $this->exequery($sql);
+
+        if ( $result->num_rows == 0 ) {
+            throw new Exception('Le produit demandé n\'existe pas en base de données');
+        }
+
+        /* Si on est là, c'est qu'on a trouvé qqch en BDD, et cette chose est normalement unique.
+         * On peut donc peupler l'objet des infos de cette chose.
+         */
+        $produit = $result->fetch_assoc();
+
+        $this->id           = $produit['id'];
+        $this->date_arrivee = $produit['date_arrivee'];
+        $this->date_depart  = $produit['date_depart'];
+        $this->prix         = $produit['prix'];
+        $this->etat         = $produit['etat'];
+        $this->salle_id     = $produit['salles_id'];
+        $this->promo_id     = $produit['promotions_id'];
     }
 
 
@@ -52,7 +75,6 @@ class Produit extends Model
     {
         return $this->id;
     }
-
 
     public function getDateArrivee()
     {
@@ -86,44 +108,41 @@ class Produit extends Model
 
     /* MUTATEURS ----------------------------------------*/
 
-    public function setDates( $date_arrivee, $date_depart )
+    public function setDateArrivee( $date_arrivee )
     {
-        if ( empty($date_arrivee) || empty($date_depart) ) {
-            return 'invalid_date';
-        }
-
         try {
             $date_arrivee = new DateTime( $date_arrivee );
         } catch (Exception $e) {
-            return 'invalid_date_arrivee';
-        }
-
-        try {
-            $date_depart = new DateTime( $date_depart );
-        } catch (Exception $e) {
-            return 'invalid_date_depart';
+            throw new Exception('Le format de la date d\'arrivée fourni est incorrect.');
         }
 
         $this->date_arrivee = $date_arrivee->format('U');
+    }
+
+    public function setDateDepart( $date_depart )
+    {
+        try {
+            $date_depart = new DateTime( $date_depart );
+        } catch (Exception $e) {
+            throw new Exception('Le format de la date de départ fourni est incorrect.');
+        }
+
         $this->date_depart  = $date_depart->format('U');
     }
 
     public function setPrix( $prix )
     {
-        if ( !empty($prix) && is_int($prix) && strlen( (string) $prix ) < 6 ) {
-            $this->prix = $prix;
-            return;
+        $prix = (int) $prix; // on s'assure de bien travailler sur un entier
+
+        if ( !$prix || strlen( (string) $prix ) < 6 ) {
+            throw new Exception('Le prix doit être un entier non-nul et ne doit pas compter plus de 6 chiffres.');
         }
 
-        return 'invalid_prix';
+        $this->prix = $prix;
     }
 
     public function setEtat( $etat )
     {
-        if ( empty($etat) ) {
-            return 'invalid_etat';
-        }
-
         if ( $etat == false ) {
             $this->etat = 0;
         } else {
@@ -133,8 +152,8 @@ class Produit extends Model
 
     public function setSalleID( $salle_id )
     {
-        if ( empty($salle_id) || !$salle_id || !is_int($salle_id) ) {
-            return 'invalid_salle_id';
+        if ( !is_int($salle_id) || !$salle_id) {
+            throw new Exception('L\'ID de la salle du produit doit être un entier non-nul');
         }
 
         $this->salle_id = $salle_id;
@@ -142,10 +161,42 @@ class Produit extends Model
 
     public function setPromoID( $promo_id )
     {
-        if ( empty($promo_id) || !$promo_id || !is_int($promo_id) ) {
-            return 'invalid_promo_id';
+        if ( !is_int($promo_id) || !$promo_id ) {
+            throw new Exception('L\'ID de la promotion du produit doit être un entier non-nul');
         }
 
         $this->promo_id = $promo_id;
+    }
+
+    /* Méthodes spécifiques -----------------------------*/
+
+    public function setProduit( $produit_data )
+    {
+        /* vérification de la présence des paramètres */
+        if (
+            !is_array( $produit_data)
+            || empty( $produit_data['date_arrivee'] )
+            || empty( $produit_data['date_depart'] )
+            || empty( $produit_data['prix'] )
+            || empty( $produit_data['etat'] )
+            || empty( $produit_data['salles_id'] )
+            || empty( $produit_data['promo_id'] )
+        ) {
+            return 'Les paramètres fournis pour le peuplement de l\'objet sont invalides ou insuffisants.';
+        }
+
+        /* on essaie de peupler le produit */
+        try {
+
+            $this->setDateArrivee( $produit_data['date_arrivee'] );
+            $this->setDateDepart( $produit_data['date_depart'] );
+            $this->setPrix( $produit_data['prix'] );
+            $this->setEtat( $produit_data['etat'] );
+            $this->setSalleID( $produit_data['salles_id'] );
+            $this->setPromoID( $produit_data['promo_id'] );
+
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
     }
 }
--- models/ProduitManager.php
+++ models/ProduitManager.php
@@ -28,19 +28,26 @@ class ProduitManager extends Model
      */
     public function __construct( $produit )
     {
-        if ( empty($produit) || !($produit instanceof Produit) ) {
+        if ( !($produit instanceof Produit) ) {
             throw new Exception('Le produit manipulé est invalide.');
         } else {
             $this->produit = $produit;
         }
     }
 
+    /*=======================================================================*/
+    /*                 Méthode principale de ProduitManager                  */
+    /*=======================================================================*/
+
     /**
-     * Ajoute un produit dans la table produits de la BDD
+     * Ajoute ou met à jour un produit dans la table produits de la BDD
      *
-     * @return string un message d'erreur en cas d'insertion impossible
+     * ProduitToDb() est protégée et ne s'emploie de l'extérieur que via des
+     * méthodes 'alias', et ce pour éviter de risquer une erreur sur le paramètre $action.
+     *
+     * @return string un message d'erreur en cas d'insertion impossible, void sinon
      */
-    public function addProduit()
+    protected function ProduitToDb( $action )
     {
         try {
             $this->checkProduit();
@@ -48,37 +55,102 @@ class ProduitManager extends Model
             return $e->getMessage();
         }
 
-        // le produit est valide, on continue
-        $sql = "INSERT INTO produits
-                VALUES ('',
-                        '" . $this->produit->getDateArrivee() . ",
-                        '" . $this->produit->getDateDepart()  . ",
-                        '" . $this->produit->getPrix()        . ",
-                        '" . $this->produit->getEtat()        . ",
-                        '" . $this->produit->getSalleID()     . ",
-                        '" . $this->produit->getPromoID()     . "');";
+        /* le produit est valide, on continue */
+        switch ( $action ) {
+            case 'insert':
+                $sql = "INSERT INTO produits
+                        VALUES ('',
+                                '" . $this->produit->getDateArrivee() . ",
+                                '" . $this->produit->getDateDepart()  . ",
+                                '" . $this->produit->getPrix()        . ",
+                                '" . $this->produit->getEtat()        . ",
+                                '" . $this->produit->getSalleID()     . ",
+                                '" . $this->produit->getPromoID()     . "');";
+                break;
+
+            case 'update':
+                $sql = "UPDATE produits
+                        SET date_arrivee='"  . $this->produit->getDateArrivee() . "',
+                            date_depart='"   . $this->produit->getDateDepart()  . "',
+                            prix='"          . $this->produit->getPrix()        . "',
+                            etat='"          . $this->produit->getEtat()        . "',
+                            salles_id='"     . $this->produit->getSalleID()     . "',
+                            promotions_id='" . $this->produit->getPromoID()     . "'
+                        WHERE id='" . $this->produit->getID() . "';";
+                break;
+
+            default: $sql = ''; // TODO: vérifier si on ne peut pas mieux faire que ça
+        }
 
         $result = $this->exequery($sql);
     }
 
+    /*=======================================================================*/
+    /*                   Alias pour l'utilisation de ProduitToDb()           */
+    /*=======================================================================*/
+
+    public function insertProduit()
+    {
+        /* Enregistrement (création) du produit dans la BDD */
+        $insert_return = $this->ProduitToDb( 'insert' );
+        if ( $insert_return ) {
+            throw new Exception( $insert_return );
+        }
+    }
+
+    public function updateProduit( $modifs )
+    {
+        /* Modification interne du produit */
+        $modif_return = $this->alterProduit( $modifs );
+        if ( $modif_return ) {
+            throw new Exception( $modif_return );
+        }
+
+        /* Enregistrement (modification) du produit dans la BDD */
+        $update_return = $this->ProduitToDb( 'update' );
+        if ( $update_return ) {
+            throw new Exception( $update_return );
+        }
+    }
+
+    /*=======================================================================*/
+    /*                 Vérifications de la cohérence du produit              */
+    /*=======================================================================*/
+
     /**
-     * Vérifie la validité du produit
+     * Vérifie que la date d'arrivée du produit est postérieure à la date du jour
      *
-     * @throws Exception si le produit ne satisfait pas toutes les conditions
+     * @throws Exception si le produit ne satisfait pas la condition
      * @return void
      */
-    public function checkProduit()
+    public function checkDateArrivee()
     {
-        /* première condition: les dates doivent être cohérentes */
         if ( date( 'Ymd', $this->produit->getDateArrivee() ) <= date( 'Ymd', time() ) ) {
             throw new Exception('La date d\'arrivée doit être postérieure à la date du jour.');
         }
+    }
 
+    /**
+     * Vérifie que la date d'arrivée du produit est antérieure à la date de départ
+     *
+     * @throws Exception si le produit ne satisfait pas cette condition
+     * @return void
+     */
+    public function checkDateRetour()
+    {
         if ( $this->produit->getDateArrivee() > $this->produit->getDateDepart() ) {
             throw new Exception('La date d\'arrivée doit être antérieure ou égale à la date de départ.');
         }
+    }
 
-        /* deuxième condition: une salle ne peut pas être utilisée par plus d'un produit à la fois */
+    /**
+     * Vérifie que la salle du produit n'est pas utilisée sur le même créneau horaire par un autre produit
+     *
+     * @throws Exception si le produit ne satisfait pas cette condition
+     * @return void
+     */
+    public function checkProduitUnique()
+    {
         $sql = "SELECT id, date_arrivee, date_depart FROM produits WHERE salles_id='" . $this->produit->getSalleID() . "';";
         $result = $this->exequery($sql);
 
@@ -96,6 +168,101 @@ class ProduitManager extends Model
 
         if ( !empty($vrai_doublon_produit) ) {
             throw new Exception('Le produit manipulé est en conflit avec les produits aux références suivantes : ' . implode(', ', $vrai_doublon_produit) . '.');
+        }
+    }
+
+    /**
+     * Vérifie que l'id de la promo du produit, s'il existe, renvoie bien à une promo existante
+     *
+     * @throws Exception si le produit ne satisfait pas cette condition
+     * @return void
+     */
+    /*public function checkPromo()
+    {
+
+    }*/
+
+    /**
+     * Teste la validité globale du produit
+     *
+     * @throws Exception si le produit ne satisfait pas toutes les conditions
+     * @return void
+     */
+    public function checkProduit()
+    {
+        /* première condition: les dates doivent être cohérentes */
+        $this->checkDateArrivee();
+        $this->checkDateRetour();
+
+        /* deuxième condition: une salle ne peut pas être utilisée par plus d'un produit à la fois */
+        $this->checkProduitUnique();
+    }
+
+    /*=======================================================================*/
+    /*                   Modifications du produit                            */
+    /*=======================================================================*/
+
+    /**
+     * Vérifie que les modifs sont valables
+     *
+     * On ne veut considérer les modifications que si elles sont cohérentes avec un Produit.
+     * D'une part, les valeurs modifiées doivent être de types scalaires et d'autre part
+     * seuls les champs suivants doivent être considérés :
+     * 'DateArrivee', 'DateDepart', 'Prix', 'Etat', 'SalleID', 'PromoID'
+     * (cad les suffixes des mutateurs de Produit).
+     *
+     * @param array le tableau des modifs souhaitées
+     * @throws Exception si les modifs ne satisfont pas tous les critères
+     * @return array le tableau des modifs valides en cas de succès
+     */
+    public function checkModifications( $modifs )
+    {
+        if ( !is_array($modifs) ) {
+            throw new Exception('Les paramètres donnés pour modification du produit sont invalides.');
+        }
+
+        $champs_valables = ['DateArrivee', 'DateDepart', 'Prix', 'Etat', 'SalleID', 'PromoID'];
+
+        /* et on supprime tous les autres */
+        foreach ($modifs as $modif_cle => $modif) {
+            if ( !in_array( $modif_cle, $champs_valables, true ) || !is_scalar( $modif ) ) {
+                unset( $modifs[$modif_cle] );
+            }
+        }
+
+        /* on vérifie qu'il reste des choses à traiter après ce nettoyage */
+        if ( empty($modifs) ) {
+            throw new Exception('Les paramètres donnés pour modification du produit sont invalides.');
+        }
+
+        return $modifs; // on peut renvoyer sereinement un tableau de modifications valides
+    }
+
+    /**
+     * Modifie le produit dont s'occupe l'instance de ProduitManager
+     *
+     * Cette méthode va appliquer les modifications demandées, si elles sont valides,
+     * à l'objet Produit dont son instance de ProduitManager s'occupe.
+     *
+     * @param array le tableau des modifs souhaitées et valables
+     * @return string un message d'erreur en cas de modification impossible, void sinon
+     */
+    public function alterProduit( $modifs )
+    {
+        /* on s'assure d'abord de la validité des modifications demandées */
+        try {
+            $modifs = $this->checkModifications( $modifs );
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        /* on altère le produit courant avec ces modifications */
+        foreach ($modifs as $modif_key => $modif) {
+            try {
+                call_user_func( [$this->produit, 'set' . $modif_key], $modif );
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
         }
     }
 }
