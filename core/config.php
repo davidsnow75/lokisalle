322341bf9b64d206ef6a962fadc3cff8be61e449
Erwan Thomas <janotus@maen.fr>
2014-10-25 16:39:52 +0200

    la branche sans rewrite tourne maintenant aussi sous nginx

--- core/config.php
+++ core/config.php
@@ -9,21 +9,23 @@
 // infos de connexion à la base de données (via un objet mysqli)
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'lokisalle');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'lokiser');
+define('DB_PASS', 'lokipass');
 
 // si le projet est en dans un sous-dossier du hostname, le définir ici (sans slash final)
-define('SUBFOLDER', '/lokisalle');
+// Exemple: define('SUBFOLDER', '/path/to/lokisalle');
+define('SUBFOLDER', false);
 
-// si le projet ne tourne pas sous rewriting d'URLs
-define('NO_REWRITE', false);
+// si le projet ne tourne pas sous rewriting d'URLs (true ou false)
+define('NO_REWRITE', true);
 
 // le contrôleur par défaut (celui de la page d'accueil)
 define('DEFAULT_CONTROLLER', 'Accueil');
 
 // souhaite-on voir les infos de debug ?
-define('DEBUG', false);
+define('DEBUG', true);
 // si oui, souhaite-on les afficher par défaut ?
 define('DEBUG_AGGRESSIF', false);
 
+// ne pas toucher svp
 setlocale (LC_TIME, 'fr_FR.utf8','fra');
