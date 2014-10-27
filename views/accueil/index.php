fdc5aef1a70073dbf3a4fe65000dd06abe2c9338
Erwan Thomas <janotus@maen.fr>
2014-10-28 00:35:06 +0100

    gestion de l'absence de produits disponibles

--- views/accueil/index.php
+++ views/accueil/index.php
@@ -5,10 +5,16 @@
 
     <div class="lgn">
       <section class="col sm8">
+
+      <?php if (empty($data['produits'])): ?>
+        <p>Aucun produit disponible pour l'instant.</p>
+      <?php else: ?>
         <p>Nos trois dernières offres&nbsp;:</p>
         <?php foreach($data['produits'] as $produit): ?>
           <?php showProduit( $produit ); ?>
         <?php endforeach; ?>
+      <?php endif; ?>
+
       </section>
 
       <aside class="col sm4">
