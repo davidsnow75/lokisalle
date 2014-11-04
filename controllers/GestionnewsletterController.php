93b63c142139bea5fcc7287edafcf3494e1fb98c
Erwan Thomas <janotus@maen.fr>
2014-11-04 17:46:16 +0100

    bugfix erreur de redirection

--- controllers/GestionnewsletterController.php
+++ controllers/GestionnewsletterController.php
@@ -22,9 +22,9 @@ class GestionnewsletterController extends AdminController
         $insertMsg = $this->loadModel('NewsletterManager')->insert( $_POST );
 
         if ( $insertMsg ) {
-            $this->quit('gestionnewsletter', 'events.gestionnewsletter.msg', 'La newsletter a bien été envoyée !');
+            $this->quit('/gestionnewsletter', 'events.gestionnewsletter.msg', 'La newsletter a bien été envoyée !');
         } else {
-            $this->quit('gestionnewsletter', 'events.gestionnewsletter.msg', 'Une erreur s\'est produite lors de la tentative d\'envoi de la newsletter');
+            $this->quit('/gestionnewsletter', 'events.gestionnewsletter.msg', 'Une erreur s\'est produite lors de la tentative d\'envoi de la newsletter');
         }
     }
 }
